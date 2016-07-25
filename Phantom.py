afe41fba79c06bca2f3d46d0e054f64ec96ff645
ravens <ravenmetals@gmail.com>
2016-07-25 18:17:10 -0500

    end of day 7/25

--- Phantom.py
+++ Phantom.py
@@ -6,6 +6,10 @@ from slacker import Slacker
 import datetime
 import time
 import sys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
 # 7/23/16 Nash
@@ -43,17 +47,18 @@ def setmarkers():
                 Marker("SEU Pika Lot", '@34.0075886500593,-96.37261748313904', "", True),
                 Marker("Casino", '@33.952295614343264,-96.41379475593567', ""),
                 Marker("Travel Plaza Lot", '@33.95312327314485,-96.41885876655579', ""),
-                Marker("1ST and Main", '@33.99229880851611,-96.37717723846436', "")
+                Marker("1ST and Main", '@33.99229880851611,-96.37717723846436', ""),
+                Marker("Wichita Falls", '@33.88962827698492,-98.5474169254303', "")
                 )
 
     return templist
 
 
 # add a slack api token here or set to none to disable
-apitoken = None  # 
+apitoken = None  #  None
 # list of pokenumbers you want to ignore see pokemon.txt for list, based off of line number ie line 1 is bulbasaur
-ignoreList = (10, 11, 13, 14, 16, 17, 18, 19, 20, 46, 47, 48, 84, 85, 127)
 base = "https://pokevision.com/#/"
+scopedistance = 325
 
 
 def doit():
@@ -63,8 +68,11 @@ def doit():
     with open(fname) as f:
         content = f.readlines()
 
+    with open('IgnoreList.txt','a+') as ignore:
+        ignorelist = ignore.read().splitlines()
+
     with open("SeenList" + str(datetime.datetime.today().day) + "-" +
-              str(datetime.datetime.today().month) + ".txt") as file:
+              str(datetime.datetime.today().month) + ".txt", "a+") as file:
         seenlist = file.readlines()
 
     for item in seenlist:
@@ -72,21 +80,28 @@ def doit():
         if itemlist[3].strip() > str(datetime.datetime.now() - datetime.timedelta(minutes=30)):
             recentlist.append(item)
             # print str(datetime.datetime.now() - datetime.timedelta(minutes=30))
-    print recentlist
     if apitoken is not None:
         slack = Slacker(apitoken)
     pokemonlist = []
+
+    dcap = dict(DesiredCapabilities.PHANTOMJS)
+    dcap["phantomjs.page.settings.userAgent"] = (
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/53 "
+        "(KHTML, like Gecko) Chrome/15.0.87"
+    )
+
     # for location in latlonlist:
     #     browser.get(base+location)
     for marker in markerList:
         try:
-            browser = webdriver.PhantomJS('E:/phantomjs-2.1.1-windows/bin/phantomjs.exe')
+            browser = webdriver.PhantomJS('E:/phantomjs-2.1.1-windows/bin/phantomjs.exe', desired_capabilities=dcap)
             browser.set_window_size(1024, 768)
-            browser.implicitly_wait(5)
-            browser.set_page_load_timeout(40)
+            browser.set_page_load_timeout(20)
             browser.get(base + marker.map)
             if 'maintenance' in browser.title.lower():
                 raise Warning('Page is down')
+            WebDriverWait(browser, 20).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "home-map-scan")))
             browser.find_element_by_class_name('home-map-scan').click()
             foo = browser.find_elements_by_class_name('leaflet-marker-icon')
             print marker.name
@@ -106,27 +121,29 @@ def doit():
 
                     for item in recentlist:
                         splititem = item.split(",")
-                        if temp.name.replace("\n", "") == splititem[0] and \
-                                str(temp.distance).replace("\n", "") == splititem[2]:
+                        if temp.name.replace("\n", "").strip() == splititem[0].strip() and \
+                                str(temp.distance).replace("\n", "").strip() == splititem[2].strip():
                             flag = True
-                            print True
-                    if not flag:
+                    if not flag and temp.distance < scopedistance:
                         # writes to a file so you can see historical data
                         with open("SeenList" + str(datetime.datetime.today().day) +
                                   "-" + str(datetime.datetime.today().month) + ".txt", "a") as myfile:
                             writestring = (temp.name + ", " + temp.marker + ", " + str(temp.distance) +
                                            ", " + datenow).replace("\n", "")
                             myfile.write(writestring + "\n")
-                    if int(key[1][0]) not in ignoreList:
+                    if key[1][0] not in ignorelist:
                         pokemonlist.append(temp)
 
+            slackmsg = ''
             for pokemon in sorted(pokemonlist):
                 # this distance sets how close a pokemon is note that this is based on screen size and not miles
-                if pokemon.distance < 300:
+                if pokemon.distance < scopedistance:
                     msg = (pokemon.name + " " + str(math.floor(pokemon.distance))).replace("\n", "")
                     print pokemon.name + " " + str(math.floor(pokemon.distance))
                     if marker.screamer and apitoken is not None:
-                        slack.chat.post_message('#pokebot', marker.name + " :: " + msg)
+                        slackmsg = slackmsg + "\t" + msg + "\n"
+            if len(slackmsg) is not 0:
+                slack.chat.post_message('#pokebot', marker.name + " :: \n" + slackmsg)
             del pokemonlist[:]
             browser.quit()
         except Exception, e:
@@ -135,7 +152,7 @@ def doit():
 
 
 if __name__ == "__main__":
-    for i in range(24):
+    for i in range(300):
         doit()
-        time.sleep(300)  # 600 every ten minutes
+        time.sleep(150)  # 600 every ten minutes
     sys.exit(0)
--- analizePokedex.py
+++ analizePokedex.py
@@ -3,14 +3,14 @@ import datetime
 with open("SeenList" + str(datetime.datetime.today().day) + "-" + str(datetime.datetime.today().month) + ".txt") as f:
         content = f.readlines()
 
-markerlist = {"SEU Dorm": {}, "SEU Pika Lot": {}, "Casino": {}, "Travel Plaza Lot": {}, "1ST and Main": {}}
-
+markerlist = {}
 for line in content:
     line.replace("\n", '')
     temp = line.split(",")
     # print temp
     foo = temp.pop(1).strip()
     boo = temp.pop(0).strip()
+    markerlist.setdefault(foo, {})
     markerlist[foo].setdefault(boo, []).append(temp)
 
 for key in markerlist:
