c01f849a26cd139052ed2cd4a69f6cc08fd43625
ravens <ravenmetals@gmail.com>
2016-07-25 23:25:34 -0500

    no more updates

--- Phantom.py
+++ Phantom.py
@@ -10,6 +10,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import random
 
 
 # 7/23/16 Nash
@@ -43,19 +44,20 @@ def setmarkers():
     # give a friendly name and the end of the url for location ie
     # https://pokevision.com/#/@34.00715285207903,-96.3775634765625 leave third value black and if you have slack
     # enabled add true to post to slack chat
-    templist = (Marker("SEU Dorm", '@34.0061355,-96.37591119999999', "", True),
-                Marker("SEU Pika Lot", '@34.0075886500593,-96.37261748313904', "", True),
-                Marker("Casino", '@33.952295614343264,-96.41379475593567', ""),
-                Marker("Travel Plaza Lot", '@33.95312327314485,-96.41885876655579', ""),
-                Marker("1ST and Main", '@33.99229880851611,-96.37717723846436', ""),
-                Marker("Wichita Falls", '@33.88962827698492,-98.5474169254303', "")
+    templist = (Marker("SEU Dorm", '@34.00613554,-96.37591119999999' + str(random.randint(1, 9)), "", True),
+                Marker("SEU Pika Lot", '@34.0075886500593,-96.37261748313904' + str(random.randint(1, 9)), "", True),
+                Marker("Casino", '@33.952295614343264,-96.41379475593567' + str(random.randint(1, 9)), ""),
+                Marker("Loves", '@33.992432239629146,-96.38123273849487' + str(random.randint(1, 9)), "", True)
+                # Marker("Travel Plaza Lot", '@33.95312327314484,-96.41885876655579' + str(random.randint(1, 9)), ""),
+                # Marker("1ST and Main", '@33.99229880851612,-96.37717723846436' + str(random.randint(1, 9)), ""),
+                # Marker("Wichita Falls", '@33.88962827698492,-98.5474169254303' + str(random.randint(1, 9)), "")
                 )
 
     return templist
 
 
 # add a slack api token here or set to none to disable
-apitoken = None  #  None
+apitoken = None
 # list of pokenumbers you want to ignore see pokemon.txt for list, based off of line number ie line 1 is bulbasaur
 base = "https://pokevision.com/#/"
 scopedistance = 325
@@ -96,18 +98,22 @@ def doit():
         try:
             browser = webdriver.PhantomJS('E:/phantomjs-2.1.1-windows/bin/phantomjs.exe', desired_capabilities=dcap)
             browser.set_window_size(1024, 768)
-            browser.set_page_load_timeout(20)
             browser.get(base + marker.map)
+            # browser.get('https://pokevision.com/#/@33.88962827698492,-98.5474169254303')
             if 'maintenance' in browser.title.lower():
                 raise Warning('Page is down')
             WebDriverWait(browser, 20).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "home-map-scan")))
-            browser.find_element_by_class_name('home-map-scan').click()
-            foo = browser.find_elements_by_class_name('leaflet-marker-icon')
+                 EC.visibility_of_element_located((By.CSS_SELECTOR, ".home-map-scan")))
+            browser.find_element_by_css_selector(".home-map-scan").click()
+            WebDriverWait(browser, 20).until(
+                EC.visibility_of_any_elements_located((By.CSS_SELECTOR, ".leaflet-marker-icon")))
+            foo = browser.find_elements_by_css_selector(".leaflet-marker-icon")
             print marker.name
+            # beautiful soup to strip out needed info.
             for fee in foo:
                 flag = False
                 boo = fee.get_attribute('src')
+                # print boo
                 if 'marker-icon.png' in boo:
                     marker.location = (int(fee.location['y']), int(fee.location['x']))
                 else:
@@ -124,30 +130,34 @@ def doit():
                         if temp.name.replace("\n", "").strip() == splititem[0].strip() and \
                                 str(temp.distance).replace("\n", "").strip() == splititem[2].strip():
                             flag = True
-                    if not flag and temp.distance < scopedistance:
-                        # writes to a file so you can see historical data
-                        with open("SeenList" + str(datetime.datetime.today().day) +
-                                  "-" + str(datetime.datetime.today().month) + ".txt", "a") as myfile:
-                            writestring = (temp.name + ", " + temp.marker + ", " + str(temp.distance) +
-                                           ", " + datenow).replace("\n", "")
-                            myfile.write(writestring + "\n")
-                    if key[1][0] not in ignorelist:
-                        pokemonlist.append(temp)
+                    recentlist.append((temp.name + ", " + temp.marker + ", " + str(temp.distance) +
+                                      ", " + datenow).replace("\n", ""))
+                    if temp.distance < scopedistance:
+                        if not flag:
+                            # writes to a file so you can see historical data
+                            with open("SeenList" + str(datetime.datetime.today().day) +
+                                      "-" + str(datetime.datetime.today().month) + ".txt", "a") as myfile:
+                                writestring = (temp.name + ", " + temp.marker + ", " + str(temp.distance) +
+                                               ", " + datenow).replace("\n", "")
+                                myfile.write(writestring + "\n")
+                        if key[1][0] not in ignorelist:
+                            pokemonlist.append(temp)
 
             slackmsg = ''
             for pokemon in sorted(pokemonlist):
                 # this distance sets how close a pokemon is note that this is based on screen size and not miles
-                if pokemon.distance < scopedistance:
-                    msg = (pokemon.name + " " + str(math.floor(pokemon.distance))).replace("\n", "")
-                    print pokemon.name + " " + str(math.floor(pokemon.distance))
-                    if marker.screamer and apitoken is not None:
-                        slackmsg = slackmsg + "\t" + msg + "\n"
+                msg = (pokemon.name + " " + str(math.floor(pokemon.distance))).replace("\n", "")
+                print pokemon.name + " " + str(math.floor(pokemon.distance))
+                if marker.screamer and apitoken is not None:
+                    slackmsg = slackmsg + "\t" + msg + "\n"
             if len(slackmsg) is not 0:
                 slack.chat.post_message('#pokebot', marker.name + " :: \n" + slackmsg)
             del pokemonlist[:]
             browser.quit()
         except Exception, e:
             print marker.name + "  " + str(e)
+            with open("errorlog.txt", "a+") as error:
+                error.writelines(marker.name + "  " + str(e) + " " + str(datetime.datetime.now()))
             browser.quit()
 
 
